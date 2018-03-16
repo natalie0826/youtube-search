bf92fcb17793127f8748ddd2d19947bb85a12c28
natallia.chobat <natashkachobot@mail.ru>
2018-03-16 19:04:49 +0300

    Adde more inforamtion to the Navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import { Search } from '../Search/Search';
 import { Selection } from '../Selection/Selection';
 import { Loading } from '../Loading/Loading';
-import './navigation.css';
+import { PageInfo } from '../PageInfo/PageInfo';
+import './Navigation.css';
 
 export const Navigation = (props) => {
     Navigation.propTypes = {
@@ -12,16 +13,27 @@ export const Navigation = (props) => {
         videoTypes: PropTypes.array.isRequired,
         videoTypeChanged: PropTypes.func.isRequired,
         updateSettings: PropTypes.func.isRequired,
-        isFetching: PropTypes.bool.isRequired
+        isFetching: PropTypes.bool.isRequired,
+        pageInfo: PropTypes.object.isRequired
     };
 
     return (
         <div className="navigation">
-            <h2>Welcome to the video app!</h2>
-            <Search searchUpdate={props.searchUpdate} />
-            <Loading loading={props.isFetching} />
-            <Selection title="Video type" items={props.videoTypes} onItemChanged={props.videoTypeChanged} />
-            <button className="save" onClick={props.updateSettings}>Update settings</button>
+            <div className="search-block">
+                <div className="title-separator">Start searching</div>
+                <Search searchUpdate={props.searchUpdate} />
+                <Loading loading={props.isFetching} />
+            </div>
+            <div className="settings-block">
+                <div className="title-separator">Settings</div>
+                <Selection title="Video type" items={props.videoTypes} onItemChanged={props.videoTypeChanged} />
+                <button className="save" onClick={props.updateSettings}>Update settings</button>
+            </div>
+            <div className="page-info-block">
+                <div className="title-separator">Page info</div>
+                <PageInfo subtitle="Per page" value={props.pageInfo.perPage} />
+                <PageInfo subtitle="Total count" value={props.pageInfo.totalCount} />
+            </div>
         </div>
     );
 }
